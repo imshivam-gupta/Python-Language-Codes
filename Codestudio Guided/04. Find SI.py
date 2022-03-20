62c03b7e2244e1a99bc527cd0601ef2aa9f388fe
Shivam Gupta <93479496+ShivamGupta2003@users.noreply.github.com>
2022-03-20 20:49:40 +0530

    Create 04. Find SI.py

---
+++ Codestudio Guided/04. Find SI.py
@@ -0,0 +1,49 @@
+# Take the principal amount, rate of interest, and the time period as input and calculate the Simple Interest.
+# Note: Return answer as Floor value.
+# Input Format:
+# The first line of input contains a single integer Principal amount. 
+# The Second line of input contains a single decimal Rate of interest.
+# The Third line of input contains a single Integer Time period.
+# Output Format:
+# Calculate the Simple Interest and print it.
+# Sample Input 1:
+# 2000
+# 2.2
+# 4
+# Sample Output 1:
+# 176
+# Explanation Of Sample Input 1:
+# The input is principal=2000, rate=2.2 and time=4.
+# So Simple interest=Principal*rate*time/100 hence 
+# answer is 2000*2.2*4/100=176
+
+#Your code goes here
+a=float(input())
+b=float(input())
+c=float(input())
+d=(a*b*c)/100
+print(int(d))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
