d80661705feddc3241c8dd25b769034de7a97812
Shivam Gupta <93479496+ShivamGupta2003@users.noreply.github.com>
2022-03-20 20:48:13 +0530

    Create 02. Print Name and Age.py

---
+++ Codestudio Guided/02. Print Name and Age.py
@@ -0,0 +1,43 @@
+# Take the person's name and age as input and print out the name and age as specified in the output format.
+# Input Format:
+# The first line of input contains a single string Name, representing the name of the person. 
+# The second line of input contains a single integer Age, representing the age of the person. 
+# Output Format:
+# The only line of output prints the name and age by sticking to the sample input format. Mind that the output string won't have preceding or trailing spaces.
+# Sample Input 1:
+# Ali
+# 30
+# Sample Output 1:
+# The name of the person is Ali and the age is 30.
+# Explanation Of Sample Input 1:
+# The input name is Ali and the input age is 30 which is printed in the specified format.
+
+a=input()
+b=int(input())
+print("The name of the person is ",a," and the age is ",b,".",sep='')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
