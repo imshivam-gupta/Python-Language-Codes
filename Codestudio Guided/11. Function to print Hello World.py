2810c36d118481a66754a274d6c985bcca0a59a8
Shivam Gupta <93479496+ShivamGupta2003@users.noreply.github.com>
2022-03-23 22:24:54 +0530

    Create 11. Function to print Hello World.py

---
+++ Codestudio Guided/11. Function to print Hello World.py
@@ -0,0 +1,13 @@
+# Write a function to print "Hello World".
+# Input Format :
+# No input required
+# Output Format :
+# The only line of output prints the string "Hello World" without any trailing and preceding white spaces or characters.
+# Sample Output 1 :
+# Hello World
+
+def printFun():
+	print("Hello World")
+
+printFun()
+
